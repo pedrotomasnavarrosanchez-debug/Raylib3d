2bb43c2d9030e2caa0ce8f0301cf1a1fe4dd9645
pedrojesussalmeroncarrion335-collab <pedrojesussalmeroncarrion335@gmail.com>
2026-07-07 13:06:17 +0200

    Añadido control de camara y gravedad

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,29 @@
-#include "raylib.h"
-#include "raymath.h"
-// Constantes de Configuración de la Ventana
-#define SCREEN_WIDTH 800  // Ancho de la ventana en píxeles
-#define SCREEN_HEIGHT 600 // Alto de la ventana en píxeles
-#define MAX_FPS 60        // Máximo de fotogramas por segundo
+    #include "raylib.h"
+    #include "raymath.h"
+    // Constantes de Configuración de la Ventana
+    #define SCREEN_WIDTH 800  // Ancho de la ventana en píxeles
+    #define SCREEN_HEIGHT 600 // Alto de la ventana en píxeles
+    #define MAX_FPS 60        // Máximo de fotogramas por segundo
 
-// Constantes de la Cámara 3D
-#define INITIAL_CAMERA_POSITION (Vector3){4.0f, 4.0f, 4.0f} // Posición inicial de la cámara (dónde está físicamente parada la cámara)
-#define INITIAL_CAMERA_TARGET (Vector3){0.0f, 0.0f, 0.0f}   // Punto exacto hacia el que está mirando el lente de la cámara
-#define INITIAL_CAMERA_UP (Vector3){0.0f, 1.0f, 0.0f}       // Hacia dónde queda el "cielo" o el techo
-#define INITIAL_CAMERA_FOVY 45.0f                           // Amplitud del lente (el ángulo de apertura vertical)
+    // Constantes de la Cámara 3D
+    #define INITIAL_CAMERA_POSITION (Vector3){4.0f, 4.0f, 4.0f} // Posición inicial de la cámara (dónde está físicamente parada la cámara)
+    #define INITIAL_CAMERA_TARGET (Vector3){0.0f, 0.0f, 0.0f}   // Punto exacto hacia el que está mirando el lente de la cámara
+    #define INITIAL_CAMERA_UP (Vector3){0.0f, 1.0f, 0.0f}       // Hacia dónde queda el "cielo" o el techo
+    #define INITIAL_CAMERA_FOVY 45.0f                           // Amplitud del lente (el ángulo de apertura vertical)
 
-// Constantes del Entorno y Objetos
-#define GRID_SLICES 20                            // Número de divisiones en la cuadrícula
-#define GRID_SPACING 1.0f                         // Espaciado entre las divisiones de la cuadrícula
-#define CUBE_POSITION (Vector3){0.0f, 1.0f, 0.0f} // Posición del cubo central en el mundo
-#define CUBE_SIZE 1.0f                            // Tamaño del cubo (Ancho, Alto, Largo)
+    // Constantes del Entorno y Objetos
+    #define GRID_SLICES 20                            // Número de divisiones en la cuadrícula
+    #define GRID_SPACING 1.0f                         // Espaciado entre las divisiones de la cuadrícula
+    #define CUBE_POSITION (Vector3){0.0f, CUBE_SIZE / 2, 0.0f} // Posición del cubo central en el mundo
+    #define CUBE_SIZE 1.0f                            // Tamaño del cubo (Ancho, Alto, Largo)
 
-// Constantes de la Interfaz (UI)
-#define TEXT_POS_X 10     // Posición X del texto en la pantalla
-#define TEXT_POS_Y 10     // Posición Y del texto en la pantalla
-#define TEXT_FONT_SIZE 20 // Tamaño de la fuente del texto
+    // Constantes de la Interfaz (UI)
+    #define TEXT_POS_X 10     // Posición X del texto en la pantalla
+    #define TEXT_POS_Y 10     // Posición Y del texto en la pantalla
+    #define TEXT_FONT_SIZE 20 // Tamaño de la fuente del texto
 
-int main()
-{
+    int main()
+    {
     // Define la pantalla principal del juego
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Mi primer juego");
 
@@ -37,27 +37,34 @@ int main()
 
     // Establece el objetivo de fotogramas por segundo de la ventana
    
-SetTargetFPS(MAX_FPS);
-// Objetos del entorno
-Vector3 posicionCubo = CUBE_POSITION;
+    SetTargetFPS(MAX_FPS);
+
+    // Objetos del entorno
+    Vector3 posicionCubo = CUBE_POSITION;
+    float gravedad = -0.009f;
+float suavidadCamara = 4.0f;
+   float velocidadPersonaje = 4.0f; // 4 cuadrados por segundo
 
 
 // TIPO VARIABLE NOMBRE;
-//TIPO VARIABLE NOMBRE = {1,2,3};
-  
-while (!WindowShouldClose())
+    //TIPO VARIABLE NOMBRE = {1,2,3};
+    
+    while (!WindowShouldClose())
     {
         // =========================================================================
         // 1. SECCIÓN DE ENTRADA (Capturar lo que hace el usuario)
         // =========================================================================
     if(IsKeyDown(KEY_A))
     {
-        posicionCubo = Vector3Add(posicionCubo, {-1.0f, 0.0f, 0.0f}); // Posicion actual, la desplazamos  
+        posicionCubo.x = posicionCubo.x + -1.0f * velocidadPersonaje * GetFrameTime();
+         // posicionCubo.x += velocidadPersonaje * GetFrameTime();  
     }
     if(IsKeyDown(KEY_D))  
     {
-    posicionCubo = Vector3Add(posicionCubo, {1.0f, 0.0f, 0.0f}); // Posicion actual, la desplazamos
-    }
+    // posicionCubo = Vector3Add(posicionCubo, {1.0f, 0.0f, 0.0f}); // Posicion actual, la desplazamos
+     posicionCubo.x = posicionCubo.x + 1.0f * velocidadPersonaje * GetFrameTime();
+}
+
    if(IsKeyDown(KEY_W))
     {
         posicionCubo = Vector3Add(posicionCubo, {0,  0,  -1}); // Posicion actual, la desplazamos  
@@ -75,8 +82,23 @@ while (!WindowShouldClose())
         // 2. SECCIÓN DE ACTUALIZACIÓN (Cálculos, físicas y lógica)
         // =========================================================================
 
+   if(posicionCubo.y > CUBE_SIZE / 2)
+   {
+     posicionCubo.y = posicionCubo.y + gravedad;
+     //equivalente a escribir -> posicionCubo.y += gravedad;
+   }
+     else
+  {
+    posicionCubo.y =  CUBE_SIZE / 2;
+  }
+
+camera.target = Vector3Lerp(camera.target, posicionCubo, suavidadCamara * GetFrameTime());
+// camera.position = Vector3Add(posicionCubo, INITIAL_CAMERA_POSITION);
+
+camera.position = Vector3Lerp(camera.position, Vector3Add(posicionCubo, INITIAL_CAMERA_POSITION), suavidadCamara * GetFrameTime());
+
         // =========================================================================
-        // 3. SECCIÓN DE RENDERIZADO (Dibujar todo en pantalla)aa
+        // 3. SECCIÓN DE RENDERIZADO (Dibujar todo en pantalla)
         // =========================================================================
         BeginDrawing();
 
